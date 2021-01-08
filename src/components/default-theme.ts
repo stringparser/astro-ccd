4c8c4c58dcc85f1b3ddd3ec9a1c9154a4f927435
Javier Carrillo Milla <stringparser@gmail.com>
2021-01-08 15:59:21 +0100

    i#3: mover estilos globales a default-theme.ts y _app.tsx

--- src/components/default-theme.ts
+++ src/components/default-theme.ts
@@ -4,17 +4,50 @@ const defaultTheme = createMuiTheme({
   palette: {
     type: 'dark',
   },
+
   typography: {
     fontFamily: '-apple-system,BlinkMacSystemFont,"Segoe UI","Roboto","Oxygen","Ubuntu","Cantarell","Fira Sans","Droid Sans","Helvetica Neue",sans-serif',
-    h1: {
-        fontSize: '3rem',
-        fontWeight: 700,
-    }
   },
 
   props: {
     MuiLink: {
       color: 'textSecondary',
+    },
+  },
+
+  overrides: {
+    MuiCssBaseline: {
+      '@global': {
+        body: {
+          margin: '0',
+          padding: '0',
+          minHeight: '100vh',
+          textAlign: 'center',
+          fontWeight: 'bold',
+
+          display: 'flex',
+          alignItems: 'center',
+          flexDirection: 'column',
+          justifyContent: 'center',
+
+          color: '#fff',
+          backgroundImage: 'url("https://astroccd.files.wordpress.com/2012/12/fondo-estrellas-tintineo.gif")',
+
+          lineHeight: '1.43',
+          textRendering: 'optimizeLegibility',
+          scrollBehavior: 'smooth',
+        },
+
+        a: {
+          color: '#ff0000',
+          fontWeight: 'bold',
+        },
+
+        img: {
+          margin: '0 auto',
+          maxWidth: '100%',
+        },
+      },
     },
   }
 });
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,7 @@
 import React from "react";
 import { create } from "jss"
 import { AppProps } from "next/app";
-import { ThemeProvider, CssBaseline, StylesProvider, jssPreset } from "@material-ui/core";
-
-import "src/css/main.css";
+import { ThemeProvider, CssBaseline, StylesProvider, jssPreset, makeStyles } from "@material-ui/core";
 
 import Navigation from "src/components/Navigation";
 import defaultTheme from "src/components/default-theme";
@@ -12,7 +10,26 @@ const jss = create({
   ...jssPreset(),
 });
 
+const useStyles = makeStyles(theme => ({
+  root: {
+    margin: '0 auto',
+    maxWidth: '960px',
+    minHeight: '100vh',
+
+    border: '0.25rem solid white',
+    borderTop: 'none',
+    borderBottom: 'none',
+    borderColor: 'rgba(255,255,255, 0.05)',
+
+    backgroundColor: 'black',
+
+    padding: '2rem 1rem',
+    paddingTop: 0,
+  }
+}));
+
 const WebApp: React.FC<AppProps> = ({ Component, pageProps }: AppProps) => {
+  const classes = useStyles();
 
   React.useEffect(() => {
     // Remove the server-side injected CSS.
@@ -25,7 +42,7 @@ const WebApp: React.FC<AppProps> = ({ Component, pageProps }: AppProps) => {
 
   return (
     <>
-      <main>
+      <main className={classes.root}>
         <ThemeProvider theme={defaultTheme}>
           <StylesProvider jss={jss}>
             <CssBaseline />
