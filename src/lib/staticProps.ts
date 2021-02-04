2060b9c28e0c7a122028ed13e04aa3dab3ba399b
Javier Carrillo Milla <stringparser@gmail.com>
2021-02-04 17:13:19 +0100

    busqueda, #9: error de path

--- src/lib/staticProps.ts
+++ src/lib/staticProps.ts
@@ -1,7 +1,7 @@
 import { RegistroItem } from 'bin/registro';
 import { mapearEntradasValidas } from './util';
 
-import registroJSON from 'datos/registro.json';
+import registroJSON from 'src/datos/registro.json';
 
 export type StaticItemsProps<T> = {
   props: {
